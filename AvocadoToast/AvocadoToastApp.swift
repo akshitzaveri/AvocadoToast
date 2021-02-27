05698fa33ce25d8f598f904b2d40b348d86e7a39
Akshit Zaveri <akshit.zaveri@gmail.com>
2021-02-27 21:01:30 +0530

    Initial Commit

---
+++ AvocadoToast/AvocadoToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoToastApp.swift
+//  AvocadoToast
+//
+//  Created by Akshit Zaveri on 27/02/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
